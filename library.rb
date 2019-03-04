be9ce54ab7349f71c7fc5841f34dd1a77ade9c87
Becky <rebeccajoynielsen@gmail.come>
2019-03-04 18:19:12 +0000

    added test/function to get book details by title

--- library.rb
+++ library.rb
@@ -1,27 +1,16 @@
-
-
-
 class Library
+  attr_reader :all_books
 
-  attr_accessor :book
-
-  def initialize(book)
-    @book = book
+  def initialize(book_details)
+    @all_books = [] << book_details
   end
 
-  def details(title)
-    if @book[:title] == title
-      return @book
+  def get_book_details(title)
+    for book in @all_books
+      if book[:title] == title
+        return book
+      end
     end
   end
-  def rental_details(title)
-    if @book[:title] == title
-      return @book[:rental_details]
-    end
-  end
-  def add_book(book_details)
-    @book.merge!(book_details)
-  end
-
 
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -3,51 +3,23 @@ require('minitest/rg')
 require_relative('../library')
 
 class TestLibrary < Minitest::Test
-  def test_check_book
-    book = Library.new(   {
-        title: "Tropic of Cancer",
-        rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
-        }
-      })
-    assert_equal({
-        title: "Tropic of Cancer",
-        rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
-        }
-      }, book.details("Tropic of Cancer"))
-  end
-  def test_rental_details
-    book = Library.new(   {
-        title: "Tropic of Cancer",
-        rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
-        }
-      })
-    assert_equal({
+  def test_return_book_details
+    library = Library.new({
+    title: "lord_of_the_rings",
+    rental_details: {
      student_name: "Jeff",
      date: "01/12/16"
-    }, book.rental_details("Tropic of Cancer"))
-  end
-  def test_add_book
-    book = Library.new(   {
-        title: "Tropic of Cancer",
-        rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
-        }
-      })
-    book.add_book( {
-        title: "Pride and Prejudice",
-        rental_details: {
-         student_name: "",
-         date: ""
-        }
-      })
-    assert_equal(2, book.length)
+    }
+  })
+
+  assert_equal({
+  title: "lord_of_the_rings",
+  rental_details: {
+   student_name: "Jeff",
+   date: "01/12/16"
+  }
+}, library.get_book_details("lord_of_the_rings"))
+
   end
 
 end
